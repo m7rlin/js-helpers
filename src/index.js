969cb326b6687c7fbb87e56417175c00455321de
m7rcin <merlin@magictm.com>
2020-02-12 06:40:17 +0100

    Add removeExtraSpaces

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ exports.ucfirst = string => {
 exports.lcfirst = string => {
   return string.charAt(0).toLowerCase() + string.slice(1);
 };
+exports.removeExtraSpaces = string => {
+  return string.replace(/\s\s+/g, " ");
+};
