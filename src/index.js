7895b8b413729bad04093d8b3656351b335dff24
m7rcin <merlin@magictm.com>
2020-02-12 06:29:08 +0100

    Add lcfirst
    First letter to lower case.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 exports.ucfirst = string => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
+exports.lcfirst = string => {
+  return string.charAt(0).toLowerCase() + string.slice(1);
+};
