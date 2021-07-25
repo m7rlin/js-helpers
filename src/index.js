12d296f8e98112a4f29d9422439a6a7b8ac793a3
shateq <69470758+shateq@users.noreply.github.com>
2021-07-25 14:58:49 +0200

    New function plus exstension to the old ones.
    > beginWithUpperCase() makes string to lower case skipping first character :D
    
    > Old names such as ucfirst and lcfirst got an update.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-exports.ucfirst = (string) => {
+exports.upperCaseFirst = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
-exports.lcfirst = (string) => {
+
+exports.beginWithUpperCase = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+}
+
+exports.lowerCaseFirst = (string) => {
   return string.charAt(0).toLowerCase() + string.slice(1)
 }
+
 exports.removeExtraSpaces = (string) => {
   return string.trim().replace(/\s\s+/g, " ")
 }
+
 exports.mapValue = (value, inMin, inMax, outMin, outMax) => {
   return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin
 }
